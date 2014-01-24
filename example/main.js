8efd4466510363ad48f809ce6f173f87380b2651
Romain Berger <romain@romainberger.com>
2014-01-24 18:30:00 +0100

    Update example

--- example/main.js
+++ example/main.js
@@ -1,11 +1,7 @@
 var voxelPainter = require('voxel-painter-core')
 
-// Colors to use
-var colors = ['2ECC71', '3498DB', '34495E', 'E67E22', 'ECF0F1']
-
 // Options
 var options = {
-    colors: colors,
     container: '#container'
 }
 
